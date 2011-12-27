2fca097e9f53c906a2f4a958aaa85b6f5cb85442
Jean-Philippe Braun <eon@patapon.info>
2011-12-27 11:10:25 +0100

    Support parentheses
    Add license header

--- extension.js
+++ extension.js
@@ -1,3 +1,18 @@
+/**
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Search = imports.ui.search;
@@ -62,10 +77,8 @@ CalcProvider.prototype = {
     },
 
     getInitialResultSet: function(terms) { 
-        let expr = "";
-        for(let i=0; i<terms.length; i++)
-           expr += terms[i]
-        if (/^[0-9.+*/-]+$/.test(expr)) {
+        let expr = terms.join('');
+        if (/^[0-9.+*/()-]+$/.test(expr)) {
             try {
                 return [{'expr': expr, 'result': eval(expr).toString()}];
             }
